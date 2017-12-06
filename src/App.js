f20a98e70c5f62ba133f1e59a9ca67681a101a86
Michael Rice <michael_rice@live.com>
2017-12-06 15:57:13 -0500

    Scaffold out redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
 import Header from './components/header'
 import UsersIndex from './components/usersIndex'
 import CreateUser from './components/createUser'
 import EditUser from './components/editUser'
 
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Switch>
-            <Route path="/users/create" component={CreateUser}  />
-            <Route path="/users/:id" component={EditUser}  />
-            <Route path="/" component={UsersIndex}  />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Switch>
+              <Route path="/users/create" component={CreateUser}  />
+              <Route path="/users/:id" component={EditUser}  />
+              <Route path="/" component={UsersIndex}  />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
